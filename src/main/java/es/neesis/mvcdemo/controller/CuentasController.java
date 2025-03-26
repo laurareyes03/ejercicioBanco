59525effbca5d3fb213553c642b9d4fd522b34ce
Tania <tania.reyes@babelgroup.com>
2025-03-26 10:43:09 +0100

    Cuentas

--- src/main/java/es/neesis/mvcdemo/controller/CuentasController.java
+++ src/main/java/es/neesis/mvcdemo/controller/CuentasController.java
@@ -1,4 +1,51 @@
 package es.neesis.mvcdemo.controller;
 
-public class CuentasController {
+import es.neesis.mvcdemo.models.Cuentas;
+import es.neesis.mvcdemo.services.ServicioCuentas;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+
+    @RestController
+    @RequestMapping("/cuentas")
+    @CrossOrigin(origins = "*") // Permite peticiones desde cualquier origen
+    public class CuentasController {
+        private final ServicioCuentas servicioCuentas;
+
+        public CuentasController(ServicioCuentas servicioCuentas) {
+            this.servicioCuentas = servicioCuentas;
+
+        }
+
+        @GetMapping
+        public List<Cuentas> listarCuentas() {
+            return servicioCuentas.listarCuentas();
+        }
+
+        @GetMapping("/{numero}")
+        public Optional<Cuentas> obtenerCuenta(@PathVariable String numero) {
+            return servicioCuentas.obtenerCuenta(numero);
+        }
+
+        @PostMapping
+        public void agregarCuenta(@RequestBody Cuentas cuenta) {
+            servicioCuentas.agregarCuenta(cuenta);
+        }
+
+        @DeleteMapping("/{numero}")
+        public void eliminarCuenta(@PathVariable String numero) {
+            servicioCuentas.eliminarCuenta(numero);
+        }
+
+        @PutMapping
+        public void actualizarCuenta(@RequestBody Cuentas cuenta) {
+            servicioCuentas.actualizarCuenta(cuenta);
+        }
 }
